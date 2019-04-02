d947ee725a58e367a3283e9e0580e84e05fe01f5
DI-Farhan <pegasus998877@gmail.com>
2019-04-02 15:00:03 +0700

    Construrtor

---
+++ Constructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa();
+            CetakHasil(mhs1);
+
+            Console.WriteLine();
+
+            Mahasiswa mhs2 = new Mahasiswa("12345", "Sukirman");
+            CetakHasil(mhs2);
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(Mahasiswa mhs)
+        {
+            Console.WriteLine("NIM : {0}", mhs.Nim);
+            Console.WriteLine("NAMA : {0}", mhs.Nama);
+        }
+    }
+}
